f012e072cfec73d42362c6cde465fddf85488df7
marmaduke <mw@eml.cc>
2014-02-04 00:10:31 +0100

    note second part of generation

---
+++ coder.py
@@ -0,0 +1,25 @@
+
+"""
+Code generation from a dag
+
+"""
+
+import dag
+
+class DAGTransform(object):
+    "Traverse graph, transform nodes & subgraphs"
+    pass
+
+class ExpandLoops(DAGTransform):
+    pass
+
+class Coder(object):
+    transforms = [ExpandLoops] # for example
+    def __call__(self, dag):
+        for t in self.transforms:
+            dag = t(dag)
+        nodes = dag.sort()
+        for n in nodes:
+            kind = n.__class__.__name__.lower()
+            yield getattr(self, 'gen_'+kind)(n)
+
