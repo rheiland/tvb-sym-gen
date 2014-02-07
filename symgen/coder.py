b0901052980ea74738c263e44b2b81818ce6ed87
Marmaduke Woodman <mmwoodman@gmail.com>
2014-02-07 13:57:18 +0100

    rewrite gen module more simply

--- symgen/coder.py
+++ symgen/coder.py
@@ -7,14 +7,14 @@ Provide base classes for code generation.
 
 """
 
+import abc
 
-class Coder(object):
-    transforms = [ExpandLoops] # for example
-    def __call__(self, dag):
-        for t in self.transforms:
-            dag = t(dag)
-        nodes = dag.sort()
-        for n in nodes:
-            kind = n.__class__.__name__.lower()
-            yield getattr(self, 'gen_'+kind)(n)
+class Gen(object):
+    """
+    Base class for code generators. 
+
+    """
+
+    __metaclass__ = abc.ABCMeta
+
 
