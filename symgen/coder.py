662fd59b6e480a72065fc226f66bc6292c16b0f1
Marmaduke Woodman <mmwoodman@gmail.com>
2014-02-07 13:52:02 +0100

    remove example transforms

--- symgen/coder.py
+++ symgen/coder.py
@@ -8,13 +8,6 @@ Provide base classes for code generation.
 """
 
 
-class DAGTransform(object):
-    "Traverse graph, transform nodes & subgraphs"
-    pass
-
-class ExpandLoops(DAGTransform):
-    pass
-
 class Coder(object):
     transforms = [ExpandLoops] # for example
     def __call__(self, dag):
