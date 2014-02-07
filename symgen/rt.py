ed70d21b109af8208771f635aabb23f244c7b74d
Marmaduke Woodman <mmwoodman@gmail.com>
2014-02-07 14:06:25 +0100

    add runtime module

---
+++ symgen/rt.py
@@ -0,0 +1,20 @@
+
+"""
+symgen.rt
+=========
+
+Runtime support for simulations.
+
+"""
+
+import abc
+
+
+class RT(object):
+    """
+    Base class for runtimes. 
+
+    """
+
+    __metaclass__ = abc.ABCMeta
+
